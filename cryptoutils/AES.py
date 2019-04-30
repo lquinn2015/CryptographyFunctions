032581f24c8e30bd8e69b928ef1b5a0c018cc4c2
lquinn <lquinn@vt.edu>
2019-04-29 22:54:28 -0400

    Challenge 18

--- cryptoutils/AES.py
+++ cryptoutils/AES.py
@@ -107,12 +107,40 @@ def union_blocks(blocks):
 
 	return out
 	
+def ctr_encrypt(s, key, nonce,size=16):
+
+    nonce = nonce.to_bytes(8,byteorder='little')
+    keystream = bytes()
+    block_count = 0
+    while len(keystream) <= len(s):
+        block = nonce + block_count.to_bytes(8, byteorder='little')
+        keystream += AES.ecb_encrypt(block, key)[:size]
+        block_count += 1
+    return XORBytes(keystream, s)
+
+def ctr_decrypt(s,key,nonce):
+    return ctr_encrypt(s,key,nonce)
+
+def XORBytes(s1,s2):
+    out = b''
+    for b1,b2 in zip(s1,s2):
+        out += bytes([b1^b2])
+    return out
+
+def test_ctr():
+    base64 = 'L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ=='
+    data = binascii.a2b_base64(base64)
+
+    print(ctr_decrypt(data, "YELLOW SUBMARINE",0))
+
+
 
 def main():
 	test_padding()
 	test_cbc()
 	s = b'a'*16 + b'b'*16 + b'c'*16 + b'd'*16
 	print(s == union_blocks(get_blocks(s)))
+	test_ctr()
 
 def test_cbc():
 	s = b'abcd' * 4 * 10 + b'a'
--- set3/CTR.py
+++ set3/CTR.py
@@ -3,13 +3,30 @@ sys.path.append('../cryptoutils/')
 import AES
 import binascii
 
-base64 = 'L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ=='
-data = binascii.a2b_base64(base64)
+def ctr_encrypt(s, key, nonce,size=16):
 
+	nonce = nonce.to_bytes(8,byteorder='little')
+	keystream = bytes()
+	block_count = 0
+	while len(keystream) <= len(s):
+		block = nonce + block_count.to_bytes(8, byteorder='little')	
+		keystream += AES.ecb_encrypt(block, key)[:size]
+		block_count += 1
+	return XORBytes(keystream, s)
 
-def ctr_encrypt(s, key, nonce):
+def ctr_decrypt(s,key,nonce):
+	return ctr_encrypt(s,key,nonce)
 
-	keystream = b''
-	needed_key = len(s)
-	
-	AES.ecb_encrypt(nonce)
+def XORBytes(s1,s2):
+	out = b''
+	for b1,b2 in zip(s1,s2):
+		out += bytes([b1^b2])
+	return out
+
+def test_ctr():
+	base64 = 'L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ=='
+	data = binascii.a2b_base64(base64)
+
+	print(ctr_decrypt(data, "YELLOW SUBMARINE",0))
+
+test_ctr()
